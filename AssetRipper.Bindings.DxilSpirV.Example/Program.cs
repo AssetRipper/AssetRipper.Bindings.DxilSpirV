6aff03ccecfa1489cc64590f3e1720679a0eea55
ds5678 <49847914+ds5678@users.noreply.github.com>
2025-04-22 19:56:47 -0700

    Add an example project

---
+++ AssetRipper.Bindings.DxilSpirV.Example/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace AssetRipper.Bindings.DxilSpirV.Example;
+
+internal class Program
+{
+	private static void Main(string[] args)
+	{
+		Logger.AddConsoleLogger();
+		byte[] data = File.ReadAllBytes(args[0]);
+		File.WriteAllBytes("output.spv", ConvertDxil(data));
+	}
+
+	private static byte[] ConvertDxil(ReadOnlySpan<byte> data)
+	{
+		using DxilBlob blob = NativeMethods.ParseDxilBlob(data);
+		using DxilConverter converter = blob.CreateConverter();
+		converter.Run();
+		ReadOnlySpan<byte> spirv = converter.GetCompiledSpirV();
+		return spirv.ToArray();
+	}
+}
