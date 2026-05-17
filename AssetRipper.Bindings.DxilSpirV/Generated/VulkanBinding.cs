a08e3d199963a5b9820cc4dc64f5699f043988cd
Jeremy Pritts <49847914+ds5678@users.noreply.github.com>
2026-05-16 18:08:34 -0700

    Update submodules (#86)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- AssetRipper.Bindings.DxilSpirV/Generated/VulkanBinding.cs
+++ AssetRipper.Bindings.DxilSpirV/Generated/VulkanBinding.cs
@@ -12,10 +12,10 @@ public partial struct VulkanBinding
     [NativeTypeName("unsigned int")]
     public uint Binding;
 
-    [NativeTypeName("__AnonymousRecord_dxil_spirv_c_L298_C2")]
+    [NativeTypeName("__AnonymousRecord_dxil_spirv_c_L301_C2")]
     public _Anonymous_e__Union Anonymous;
 
-    [NativeTypeName("__AnonymousRecord_dxil_spirv_c_L310_C2")]
+    [NativeTypeName("__AnonymousRecord_dxil_spirv_c_L313_C2")]
     public _Bindless_e__Struct Bindless;
 
     [NativeTypeName("dxil_spv_vulkan_descriptor_type")]
